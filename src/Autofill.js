e64bf53b2d5cd5f9afa0afabe98f27124208be9d
Helder <he7d3r@gmail.com>
2013-12-14 11:56:29 +0000

    Prefill the label in the current user language using one of the existing fallbacks
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/Autofill.js
@@ -0,0 +1,42 @@
+/**
+ * Prefill the label in the current user language using one of the existing fallbacks
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Autofill.js]] ([[File:User:Helder.wiki/Tools/Autofill.js]])
+ */
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+	var fallbacks = {
+			'pt-br': [ 'pt', 'es', 'gl' ],
+			'pt': [ 'pt-br', 'es', 'gl' ]
+		};
+ 
+	/**
+	 * Create the links
+	 */
+	function init() {
+		var uLang = mw.config.get( 'wgUserLanguage' ),
+			i, fallback;
+		if ( !fallbacks[ uLang ] || !fallbacks[ uLang ].length ){
+			return;
+		}
+		for ( i = 0; i < fallbacks[ uLang ].length; i++ ){
+			fallback = $( '.wb-terms-label-' + fallbacks[ uLang ][i] )
+				.text().replace( /^\s+|\s+$/g, '' );
+			if ( fallback ){
+				break;
+			}
+		}
+		if ( fallback ){
+			$( 'h1' )
+				.find( 'input' )
+					.val( fallback )
+					.trigger( 'input' )
+					.focus();
+		}
+	}
+ 
+	$( init );
+
+}( mediaWiki, jQuery ) );
