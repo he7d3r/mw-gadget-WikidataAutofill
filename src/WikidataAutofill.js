c2fe658a3a66884196c23519145adb20829aa75f
Helder <he7d3r@gmail.com>
2014-09-20 11:22:29 -0300

    Remove tracking link (only useful on wiki)

--- src/WikidataAutofill.js
+++ src/WikidataAutofill.js
@@ -2,7 +2,6 @@
  * Prefill the label in the current user language using one of the existing fallbacks
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Autofill.js]] ([[File:User:Helder.wiki/Tools/Autofill.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
