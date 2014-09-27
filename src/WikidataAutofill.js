dd5772a932042ac5cddf7797958384e5922c3246
Helder <he7d3r@gmail.com>
2014-09-27 20:49:04 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/WikidataAutofill.js
+++ src/WikidataAutofill.js
@@ -4,12 +4,12 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 	var fallbacks = {
 			'pt-br': [ 'pt', 'es', 'gl' ],
 			'pt': [ 'pt-br', 'es', 'gl' ]
 		};
- 
+
 	/**
 	 * Create the links
 	 */
@@ -17,23 +17,23 @@
 		var uLang = mw.config.get( 'wgUserLanguage' ),
 			$input = $( 'h1' ).find( 'input' ),
 			i, fallback;
-		if ( !fallbacks[ uLang ] || !fallbacks[ uLang ].length || !$input.length ){
+		if ( !fallbacks[ uLang ] || !fallbacks[ uLang ].length || !$input.length ) {
 			return;
 		}
-		for ( i = 0; i < fallbacks[ uLang ].length; i++ ){
+		for ( i = 0; i < fallbacks[ uLang ].length; i++ ) {
 			fallback = $( '.wb-terms-label-' + fallbacks[ uLang ][i] )
 				.text().replace( /^\s+|\s+$/g, '' );
-			if ( fallback ){
+			if ( fallback ) {
 				break;
 			}
 		}
-		if ( fallback ){
+		if ( fallback ) {
 			$input.val( fallback )
 				.trigger( 'input' )
 				.focus();
 		}
 	}
- 
+
 	$( init );
 
 }( mediaWiki, jQuery ) );
